077a6a32226cc7753a8f659612b438bb1c8f3e4d
“your_username” <67698619@qq.com>
2019-10-26 18:13:28 +0800

    开始集成项目

--- app/src/main/java/com/github/YspDemo/Main2Activity.java
+++ app/src/main/java/com/github/YspDemo/Main2Activity.java
@@ -1,73 +1,186 @@
 package com.github.YspDemo;
 
-import android.content.Intent;
-import android.content.res.AssetFileDescriptor;
-import android.database.Cursor;
-import android.net.Uri;
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.graphics.ImageFormat;
+import android.graphics.SurfaceTexture;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraCaptureSession;
+import android.hardware.camera2.CameraCharacteristics;
+import android.hardware.camera2.CameraDevice;
+import android.hardware.camera2.CameraManager;
+import android.hardware.camera2.CaptureRequest;
+import android.hardware.camera2.TotalCaptureResult;
+import android.media.Image;
+import android.media.ImageReader;
+import android.os.Build;
 import android.os.Bundle;
-import android.os.Environment;
-import android.provider.MediaStore;
-import android.support.annotation.Nullable;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Surface;
+import android.view.TextureView;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
 
-import com.github.YspDemo.jni.FFmpegCmd;
-import com.github.YspDemo.util.MediaTool;
+import java.util.Arrays;
 
-import java.io.File;
+public class Main2Activity extends AppCompatActivity implements View.OnClickListener {
+    private Button record;
+    private TextureView textureView;
+    private CameraManager cameraManager;
+    private String cameraID;
+    private static final String TAG = "maff";
+    private CameraDevice.StateCallback deviceStateCall;
+    private TextureView.SurfaceTextureListener surfaceTextureListener;
+    private CameraDevice cameraDevice;
+    private Surface surface1;
+    private boolean recording = false;
+    ImageReader nv21Reader;
 
-public class Main2Activity extends AppCompatActivity {
-    private Button ffmpegButton;
-    private Button shuaxing;
-    private TextView ffmpegText;
-    private static final int MAFF = 0x2;
-    private String vidioPath;
-    private String outVidioPath;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
-        ffmpegButton = findViewById(R.id.button_ffmpeg);
-        shuaxing = findViewById(R.id.button_ffmpeg2);
-        ffmpegText = findViewById(R.id.text_ffmpeg);
-        ffmpegButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent i = new Intent(Intent.ACTION_PICK, MediaStore.Video.Media.EXTERNAL_CONTENT_URI);
-                startActivityForResult(i, MAFF);
-            }
-        });
-        shuaxing.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                MediaTool.insertMedia(getApplicationContext(),outVidioPath);
-            }
-        });
-        File file = new File(Environment.getExternalStorageDirectory().getAbsolutePath() + "/ffmpeg" );
-        if (!file.exists()){
-            file.mkdirs();
+        init();
+    }
+
+    private void init() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                            Manifest.permission.READ_EXTERNAL_STORAGE,
+                            Manifest.permission.CAMERA,
+                    },
+                    0);
         }
-        File extractorFile = new File(file,"ffmpegOut.mp4");
-        outVidioPath = extractorFile.getAbsolutePath();
+        record = findViewById(R.id.button_recorded);
+        record.setOnClickListener(this);
+        textureView = findViewById(R.id.texture);
+        textureView.setDrawingCacheEnabled(false);
+        textureView.setSurfaceTextureListener(new TextureView.SurfaceTextureListener() {
+            @Override
+            public void onSurfaceTextureAvailable(SurfaceTexture surface, int width, int height) {
+                Log.d(TAG, "onSurfaceTextureAvailable: ");
+                surface1 = new Surface(surface);
+                nv21Reader = ImageReader.newInstance(width, height, ImageFormat.YUV_420_888, 2);
+                nv21Reader.setOnImageAvailableListener(new ImageReader.OnImageAvailableListener() {
+                    @Override
+                    public void onImageAvailable(ImageReader reader) {
+                        //nv21格式处理相关
+                        Image image = reader.acquireLatestImage();
+                        if (image != null) {
+                            image.close();
+                        }
+                    }
+                }, null);
+            }
+
+            @Override
+            public void onSurfaceTextureSizeChanged(SurfaceTexture surface, int width, int height) {
+
+            }
+
+            @Override
+            public boolean onSurfaceTextureDestroyed(SurfaceTexture surface) {
+                return false;
+            }
+
+            @Override
+            public void onSurfaceTextureUpdated(SurfaceTexture surface) {
+
+            }
+        });
+        try {
+            cameraManager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
+            String[] cameraIdList;//手机的相机列表
+            cameraIdList = cameraManager.getCameraIdList();
+            for (String s : cameraIdList) {//找到后置摄像头
+                CameraCharacteristics cameraCharacteristics = cameraManager.getCameraCharacteristics(s);
+                if (cameraCharacteristics.get(CameraCharacteristics.LENS_FACING) == CameraCharacteristics.LENS_FACING_BACK) {
+                    cameraID = s;
+                    break;
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        deviceStateCall = new CameraDevice.StateCallback() {
+            @Override
+            public void onOpened(@NonNull CameraDevice camera) {
+                cameraDevice = camera;
+                final CaptureRequest.Builder captureRequest;
+                try {
+                    captureRequest = cameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW);
+                    captureRequest.addTarget(surface1);
+                    captureRequest.addTarget(nv21Reader.getSurface());
+                    cameraDevice.createCaptureSession(Arrays.asList(surface1, nv21Reader.getSurface()), new CameraCaptureSession.StateCallback() {
+                        @Override
+                        public void onConfigured(@NonNull CameraCaptureSession session) {
+                            CameraCaptureSession session1 = session;
+                            try {
+                                session1.setRepeatingRequest(captureRequest.build(), new CameraCaptureSession.CaptureCallback() {
+                                    @Override
+                                    public void onCaptureCompleted(@NonNull CameraCaptureSession session, @NonNull CaptureRequest request, @NonNull TotalCaptureResult result) {
+                                        super.onCaptureCompleted(session, request, result);
+                                    }
+                                }, null);
+                            } catch (CameraAccessException e) {
+                                e.printStackTrace();
+                            }
+                        }
+
+                        @Override
+                        public void onConfigureFailed(@NonNull CameraCaptureSession session) {
+
+                        }
+                    }, null);
+                } catch (CameraAccessException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onDisconnected(@NonNull CameraDevice camera) {
+
+            }
+
+            @Override
+            public void onError(@NonNull CameraDevice camera, int error) {
+
+            }
+        };
+    }
+
+
+    @SuppressLint("MissingPermission")
+    private void openCamera() {
+        try {
+            cameraManager.openCamera(cameraID, deviceStateCall, null);
+            record.setText("停止录制");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void stopCamera() {
+        cameraDevice.close();
+        record.setText("开始录制");
     }
 
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        if (requestCode == MAFF && resultCode == RESULT_OK && null != data) {
-            Uri selectedVideo = data.getData();
-            String[] filePathColumn = {MediaStore.Video.Media.DATA};
-
-            Cursor cursor = getContentResolver().query(selectedVideo,
-                    filePathColumn, null, null, null);
-            cursor.moveToFirst();
-
-            int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
-            vidioPath = cursor.getString(columnIndex);
-            cursor.close();
-            ffmpegText.setText(vidioPath);
-            FFmpegCmd.look(vidioPath,outVidioPath);
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.button_recorded:
+                if (!recording) {
+                    recording = true;
+                    openCamera();
+                } else {
+                    recording = false;
+                    stopCamera();
+                }
+                break;
         }
     }
 }
